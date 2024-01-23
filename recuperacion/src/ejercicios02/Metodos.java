efb34bf290fe9856b206f93f1d2106d38f118329
Jo1aw3 <joshuabotello2003@gmail.com>
2024-01-23 14:21:51 +0100

    up

--- recuperacion/src/ejercicios02/Metodos.java
+++ recuperacion/src/ejercicios02/Metodos.java
@@ -9,30 +9,28 @@ public class Metodos {
         return cantidadPesetas;
     }
 	
-	public static void euroDolar(double cantidadDinero, char monedaCambio) {
+	public static double euroDolar(double cantidadDinero, char monedaCambio) {
 		double cambioEuro = 0.92; 
 		double cambioDolar = 1.09;
-		// monedaCambio.toLowerClass;
+		double cantidadCambio = 0;
 		
+		monedaCambio=Character.toLowerCase(monedaCambio);
 		if (monedaCambio == 'e') {
 			System.out.println("la cantidad esta en dolar.. pasando a euro");
-			double cantidadCambio = cantidadDinero * cambioEuro;
-			return cantidadCambio;
+			cantidadCambio = cantidadDinero * cambioEuro;
 		} else if (monedaCambio == 'd'){
 			System.out.println("la cantidad esta en euro.. pasando a dolar");
-			double cantidadCambio = cantidadDinero * cambioDolar;
-			return cantidadCambio;
+			cantidadCambio = cantidadDinero * cambioDolar;
 		} else {
 			System.out.println("caracter desconocido");
 		}
-		
+		return cantidadCambio;
 	}
 
 	public static void main(String[] args) {
 
 		// variables
 		double euros = 70.0;
-		double dolares = 50.0;
 		
 		// 1. euros a pesetas
 		// double cantidadPesetas = Metodos.eurosPesetas(euros); si estuvieramos en otra clase
@@ -41,8 +39,9 @@ public class Metodos {
 		System.out.println(euros + " Euros equivalen a " + pesetas + " Pesetas");
 		
 		// 2.1 euros a dolares
-		double 
-		
+		System.out.println(euroDolar(40,'d'));
+		double cambio = euroDolar(40,'e');
+		System.out.println(cambio);
 		
 		// 2.2 dolares a euros
 		
